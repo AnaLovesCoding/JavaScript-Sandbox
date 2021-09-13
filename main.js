619c269a600adf2fb33cb3f92ee13491b6bde0cb
Sachin Hiriyanna <gvarchana95@gmail.com>
2021-09-13 16:13:12 -0400

    updated main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //alert("hello123");
-console.log('Hello');
+console.log("Hello");
 // https://developer.mozilla.org/en-US/docs/Web/API/console
 //console.error("this is an error");
 //console.warn("this is warning");
@@ -21,52 +21,56 @@ let z;
 console.log(typeof z);
 
 //concatenate
-console.log('My name is ' + name + ' and i am ' + age);
+console.log("My name is " + name + " and i am " + age);
 console.log(`My name is ${name} and I am ${age}`);
 
 const s = "Hello world";
-console.log(s.substring(0,7).toUpperCase());
-console.log(s.split(''));
+console.log(s.substring(0, 7).toUpperCase());
+console.log(s.split(""));
 
-const t = 'technology, computer, it, code';
-console.log(t.split(', '));
+const t = "technology, computer, it, code";
+console.log(t.split(", "));
 
 //Arrays
 
-const numbers = new Array(1,2,3,4,5);
+const numbers = new Array(1, 2, 3, 4, 5);
 console.log(numbers);
 
-const fruits = ['apples','oranges', 10]
-fruits[3]='pears';
-fruits.push('mangos');
-fruits.unshift('strawberries');
+const fruits = ["apples", "oranges", 10];
+fruits[3] = "pears";
+fruits.push("mangos");
+fruits.unshift("strawberries");
 fruits.pop();
 console.log(Array.isArray(fruits));
 console.log(fruits);
-console.log(fruits.includes('mangos'));
-console.log(fruits.indexOf('oranges'));
+console.log(fruits.includes("mangos"));
+console.log(fruits.indexOf("oranges"));
 
 //Object literals
 const person = {
-    firstName: 'Archana',
-    lastName: 'Sachin',
-    age: 26,
-    hobbies: ['music', 'movies', 'sports'],
-    address: {
-        street: '25 Selby Street',
-        city: 'Toronto',
-        state: 'ON'
-    }
-}
+  firstName: "Archana",
+  lastName: "Sachin",
+  age: 26,
+  hobbies: ["music", "movies", "sports"],
+  address: {
+    street: "25 Selby Street",
+    city: "Toronto",
+    state: "ON",
+  },
+};
 console.log(person.firstName, person.lastName);
 console.log(person.hobbies[1]);
 console.log(person.address.street);
 
-const {firstName, lastName, address: { city }} = person;
+const {
+  firstName,
+  lastName,
+  address: { city },
+} = person;
 console.log(firstName);
 console.log(city);
 
-person.email = 'archana@gmail.com'
+person.email = "archana@gmail.com";
 console.log(person.email);
 
 const todos = [
@@ -117,16 +121,18 @@ todos.forEach((todo) => {
 });
 
 const todoText = todos.map(function (todo) {
-    return todo.text;
-  });
-  console.log(todoText);
+  return todo.text;
+});
+console.log(todoText);
 
-  const todoCompletedIds = todos.filter(function (todo) {
+const todoCompletedIds = todos
+  .filter(function (todo) {
     return todo.isCompleted === true; //== is datatype insensitive
-  }).map(function(todo){
-      return todo.id;
+  })
+  .map(function (todo) {
+    return todo.id;
   });
-  console.log(todoCompletedIds);
+console.log(todoCompletedIds);
 
 // const x = "10";
 // if( x === 10){
@@ -138,84 +144,82 @@ const todoText = todos.map(function (todo) {
 
 const b = 11;
 
-const color = b > 10 ? 'red' : 'blue';
+const color = b > 10 ? "red" : "blue";
 console.log(color);
 
-switch(color){
-    case 'red':
-        console.log('color is red');
-        break;
-    case 'blue':
-        console.log('color is blue');
-        break;
-    default :
-        console.log('colors is not red or blue');
-        break;
-
+switch (color) {
+  case "red":
+    console.log("color is red");
+    break;
+  case "blue":
+    console.log("color is blue");
+    break;
+  default:
+    console.log("colors is not red or blue");
+    break;
 }
 
-function add(num1, num2){
-    return (num1 + num2);
+function add(num1, num2) {
+  return num1 + num2;
 }
-console.log(add(1,1));
+console.log(add(1, 1));
 
 const addNums = (num1 = 3, num2 = 3) => num1 + num2;
 const addNums2 = (num1 = 3, num2 = 4) => {
-    return  num1 + num2;
+  return num1 + num2;
 };
 
 console.log(addNums());
 
-const addNums3 = num1 => num1 + 5;
+const addNums3 = (num1) => num1 + 5;
 console.log(addNums3(5));
 
 //constructor function
-function Person(firstName, lastName, dob){
+function Person(firstName, lastName, dob) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.dob = new Date(dob);
+
+  this.getFullName = function () {
+    return `${this.firstName} ${this.lastName}`;
+  };
+}
+
+Person.prototype.getBirthYear = function () {
+  return this.dob.getFullYear();
+};
+
+//class
+class Persons {
+  constructor(firstName, lastName, dob) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.dob = new Date(dob);
-
-    this.getFullName = function(){
-        return `${this.firstName} ${this.lastName}`;
-
-    }
-}
-
-Person.prototype.getBirthYear = function(){
+  }
+  getBirthYear() {
     return this.dob.getFullYear();
-}
-
-//class
-class Persons{
-    constructor(firstName, lastName, dob){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.dob = new Date(dob);
-    }
-    getBirthYear(){
-        return this.dob.getFullYear();
-    }
-    getFullName(){
-        return `${this.firstName} ${this.lastName}`;
-    }
+  }
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
 }
 
 //Instantiate Object
-const person1 = new Person('Archana', 'Sachin', '5-17-1995');
+const person1 = new Person("Archana", "Sachin", "5-17-1995");
 console.log(person1.getBirthYear());
 console.log(person1.getFullName());
 console.log(person1);
 
 //single element
-console.log(document.getElementById('my-form'));
-console.log(document.querySelector('h1'));
+console.log(document.getElementById("my-form"));
+console.log(document.querySelector("h1"));
 
 //Multiple element
-console.log(document.querySelectorAll('.item'));
-console.log(document.getElementsByClassName('item'));
-console.log(document.getElementsByTagName('li'));
+console.log(document.querySelectorAll(".item"));
+console.log(document.getElementsByClassName("item"));
+console.log(document.getElementsByTagName("li"));
 
-const items = document.querySelectorAll('.item');
+const items = document.querySelectorAll(".item");
 console.log(items);
 items.forEach((item) => console.log(item));
 
@@ -226,16 +230,16 @@ const ul = document.querySelector(".items");
 //ul.children[1].innerText = "Archana";
 //ul.lastElementChild.innerHTML = '<h1>Sachin</h1>';
 
-const btn = document.querySelector('.btn');
-btn.style.background = 'red';
+const btn = document.querySelector(".btn");
+btn.style.background = "red";
 
 //const btn = document.querySelector('.btn');
 
-btn.addEventListener('mouseout', (e) =>{
-    e.preventDefault();
-    //console.log(e.target.className);
-    document.querySelector('#my-form').style.background = '#ccc';
-    document.querySelector('body').classList.add('bg-dark');
+btn.addEventListener("mouseout", (e) => {
+  e.preventDefault();
+  //console.log(e.target.className);
+  document.querySelector("#my-form").style.background = "#ccc";
+  document.querySelector("body").classList.add("bg-dark");
 });
 
 const myForm = document.querySelector("#my-form");
@@ -250,17 +254,20 @@ function onSubmit(e) {
   e.preventDefault();
   //console.log(nameInput.value);
   if (nameInput.value === "" || emailInput.value === "") {
-      msg.innerText = "Please enter name and email";
-      //msg.classList.add('error');
+    msg.style.display = "block";
+    msg.innerText = "Please enter name and email";
+    msg.classList.add("error");
 
-      setTimeout(() => msg.remove(), 3000);
-  }else{
-      const li = document.createElement('li');
-      li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
-      userList.appendChild(li);
+    setTimeout(() => (msg.style.display = "none"), 3000);
+  } else {
+    const li = document.createElement("li");
+    li.appendChild(
+      document.createTextNode(`${nameInput.value} : ${emailInput.value}`)
+    );
+    userList.appendChild(li);
 
-      //clear fields
-      nameInput.value = '';
-      emailInput.value = '';
+    //clear fields
+    nameInput.value = "";
+    emailInput.value = "";
   }
 }
